1f1ad13aa16b649ba753a5921c0d17b69a5fde11
MatthiasChometon <matthias.chometon@ynov.com>
2020-11-27 16:42:08 +0100

    fix structure

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    test home
+    test main
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Main",
   data() {
     return {};
   },
