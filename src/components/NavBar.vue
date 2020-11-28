738c1745eec15c723bbc5c92d0cff763acb1fd1d
johannadznd <johanna.dezarnaud@ynov.com>
2020-11-28 12:47:48 +0100

    page home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+     <p>test</p>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+  }
+}
+};
+</script>
+
+<style scoped>
+.clipped {
+    clip-path: polygon(100% 100%, 81% 79%, 57% 79%, 35% 82%, 23% 74%, 0 100%, 0 0, 100% 0);
+
+
+    height: 9vw;
+
+
+    background: #32374B;
+}
+
+.logo{
+
+    width: 8vw;
+    padding:2vw;
+}
+</style>
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
+    <NavBar></NavBar>
     test main
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
+
 export default {
   name: "Main",
   data() {
     return {};
+  },
+  components: {
+    NavBar ,
   },
 };
 </script>
