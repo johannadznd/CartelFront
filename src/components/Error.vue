db51d257a7921ca6f8a2721a194ddeb04ffb76d2
MatthiasChometon <matthias.chometon@ynov.com>
2020-12-07 15:04:18 +0100

    fix Error.vue

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,17 +1,40 @@
 <template>
-  <v-alert class="ma-2" elevation="11" type="error" dismissible v-if="error.length != 0"
-    >erreur {{ error.data.status }} : {{ error.data.error }}</v-alert
+  <v-alert
+    class="ma-2 white--text"
+    elevation="11"
+    color="red"
+    v-if="error.length != 0"
   >
+    <div class="d-flex align-center">
+      <v-icon class="mr-2 ml-2" color="white">mdi-alert</v-icon>
+      erreur {{ error.data.status }} : {{ error.data.error }}
+      <v-spacer class="d-flex justify-end">
+        <v-btn @click="resetError()" icon color="white"
+          ><v-icon> mdi-close </v-icon></v-btn
+        >
+      </v-spacer>
+    </div>
+  </v-alert>
 </template>
 
 <script>
 import store from "@/store/index.js";
 export default {
+  watch: {
+    $route(to, from) {
+      this.resetError();
+    },
+  },
+  methods: {
+    resetError() {
+      store.commit("setError", []);
+    },
+  },
   name: "Error",
   store: store,
   computed: {
     error() {
-      if (store.state.error) {
+      if (store.state.error != []) {
         return store.state.error;
       }
       return "";
